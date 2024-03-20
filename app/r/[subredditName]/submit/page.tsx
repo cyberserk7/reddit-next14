009ed3f815161ad9b902f0f74e088733bfa170bf
cyberserk7 <deynilabjo@gmail.com>
2024-03-21 01:01:56 +0530

    loader fix

--- app/r/[subredditName]/submit/page.tsx
+++ app/r/[subredditName]/submit/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
 import CreatePostSection from "./_components/create-post-section";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Loader2 } from "lucide-react";
 
 const CreatePostPage = ({
     params
@@ -10,8 +12,11 @@ const CreatePostPage = ({
 }) => {
   return (
     <Suspense fallback={
-        <div className="w-full">
+        <div className="w-full lg:w-[65%]">
             <h1 className="text-2xl font-medium">Create a post</h1>
+            <div className="h-80 w-full flex items-center justify-center">
+                <Loader2 className="w-10 h-10 text-muted-foreground animate-spin" />
+            </div>
         </div>
     }>
         <CreatePostSection subredditName={params.subredditName} />
