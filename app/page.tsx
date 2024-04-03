fd4cb4ea772bde5025c9bc4f36c3f42a77c6597b
cyberserk7 <deynilabjo@gmail.com>
2024-04-03 09:49:43 +0530

    fixed the homescreen height constraint bug

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,19 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="w-full h-full flex justify-between">
-      <div className="w-full lg:w-[65%] h-full">
-        <Suspense fallback={
-          <FeedSkeleton isSubredditPage={false} />
-        }>
+    <main className="w-max h-max flex justify-between">
+      <div className="w-full lg:w-[65%] h-max">
+        <Suspense fallback={<FeedSkeleton isSubredditPage={false} />}>
           <HomeFeed />
         </Suspense>
       </div>
-      <Suspense fallback ={
-        <div className="w-80 h-80 hidden lg:block">
-          <Skeleton className="w-full h-full bg-skeleton">
-          </Skeleton>
-        </div>
-      }>
+      <Suspense
+        fallback={
+          <div className="w-80 h-80 hidden lg:block">
+            <Skeleton className="w-full h-full bg-skeleton"></Skeleton>
+          </div>
+        }
+      >
         <FeedInfoCard />
       </Suspense>
     </main>
